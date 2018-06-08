9d239a1e39dcfd6ac58515002afcfbec3721ad3b
Parthiban Sugumar <parthibansugumar66@gmail.com>
2018-06-08 22:05:28 +0530

    Auto formatted the text

--- src/svg.js
+++ src/svg.js
@@ -14,34 +14,34 @@ class Svg extends Component {
     componentDidUpdate() {
         this.rotateLogo();
     }
-    scale= Math.min(Math.floor(this.props.totalHeight / this.props.LayoutHeight),
+    scale = Math.min(Math.floor(this.props.totalHeight / this.props.LayoutHeight),
         Math.floor(this.props.totalWidth / this.props.LayoutWidth));
     render() {
         return <svg ref={node => this.node = node}
-        height={this.props.totalHeight.toString()}
-        width={this.props.totalWidth.toString()}>
-        {
-            this.props.machine.map(machine =>
-            <svg>
-                <svg>
-                     <circle
-                        cx={(machine.pointX + machine.machineWidth) * this.scale}
-                        cy={machine.pointY * this.scale}
-                        r="10"
-                        style={
-                            machine.machineStatus == 1 ?
-                            safeStyle : unSafeStyle
-                        }/>
-                </svg>
-                <svg
-                width={ (machine.machineWidth * this.scale).toString()}
-                height={ (machine.machineHeight * this.scale).toString()}
-                x={ (machine.pointX * this.scale).toString()}
-                y={ (machine.pointY * this.scale).toString()}
-                dangerouslySetInnerHTML={{ __html: machine.svgString }}/>
-            </svg>
-            )
-        }
+            height={this.props.totalHeight.toString()}
+            width={this.props.totalWidth.toString()}>
+            {
+                this.props.machine.map(machine =>
+                    <svg>
+                        <svg>
+                            <circle
+                                cx={(machine.pointX + machine.machineWidth) * this.scale}
+                                cy={machine.pointY * this.scale}
+                                r="10"
+                                style={
+                                    machine.machineStatus == 1 ?
+                                        safeStyle : unSafeStyle
+                                } />
+                        </svg>
+                        <svg
+                            width={(machine.machineWidth * this.scale).toString()}
+                            height={(machine.machineHeight * this.scale).toString()}
+                            x={(machine.pointX * this.scale).toString()}
+                            y={(machine.pointY * this.scale).toString()}
+                            dangerouslySetInnerHTML={{ __html: machine.svgString }} />
+                    </svg>
+                )
+            }
         </svg>
     }
     rotateLogo() {
