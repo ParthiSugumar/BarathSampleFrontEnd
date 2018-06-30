41bfd691bf9dc4d69fd0fb783cea0130d1e798cd
Parthiban Sugumar <parthibansugumar66@gmail.com>
2018-06-30 12:31:54 +0530

    d3 zooming completed 90%

--- src/svg.js
+++ src/svg.js
@@ -8,12 +8,6 @@ var unSafeStyle = {
     fill: 'red'
 }
 class Svg extends Component {
-    // componentDidMount() {
-    //     this.rotateLogo();
-    // }
-    // componentDidUpdate() {
-    //     this.rotateLogo();
-    // }
     scale = Math.min(Math.floor(this.props.totalHeight / this.props.LayoutHeight),
         Math.floor(this.props.totalWidth / this.props.LayoutWidth));
     render() {
@@ -22,37 +16,50 @@ class Svg extends Component {
             width={this.props.totalWidth.toString()}>
             {
                 this.props.machine.map(machine =>
-                    <g onClick={this.rotateLogo}>
-                        <svg>
+                    <svg>
+                        <g onMouseOver={() => this.rotateLogo(machine.machineID)}>
                             <svg
                                 id={machine.machineID}
                                 width={(machine.machineWidth * this.scale).toString()}
                                 height={(machine.machineHeight * this.scale).toString()}
                                 x={(machine.pointX * this.scale).toString()}
                                 y={(machine.pointY * this.scale).toString()}
+                                onMouseLeave={() => this.returnLogo(machine.machineID,
+                                    (machine.pointX * this.scale).toString(),
+                                    (machine.pointY * this.scale).toString(),
+                                    (machine.machineHeight * this.scale).toString(),
+                                    (machine.machineWidth * this.scale).toString()
+                                )}
                                 dangerouslySetInnerHTML={{ __html: machine.svgString }} />
-                            <svg>
-                                <circle
-                                    cx={(machine.pointX + machine.machineWidth) * this.scale}
-                                    cy={machine.pointY * this.scale}
-                                    r="10"
-                                    style={
-                                        machine.machineStatus == 1 ?
-                                            safeStyle : unSafeStyle
-                                    } />
-                            </svg>
+                        </g>
+                        <svg>
+                            <circle
+                                cx={(machine.pointX + machine.machineWidth) * this.scale}
+                                cy={machine.pointY * this.scale}
+                                r="10"
+                                style={
+                                    machine.machineStatus == 1 ?
+                                        safeStyle : unSafeStyle
+                                } />
                         </svg>
-                    </g>
+                    </svg>
                 )
             }
         </svg>
     }
-    rotateLogo() {
-        d3.select("#machine-4")
+    rotateLogo(id) {
+        d3.select("#" + id)
             .attr("x", 0)
             .attr("y", 0)
             .attr("width", "100%")
             .attr("height", "100%")
+    }
+    returnLogo(id, px, py, h, w) {
+        d3.select("#" + id)
+            .attr("x", px)
+            .attr("y", py)
+            .attr("height", h)
+            .attr("width", w)
     }
 }
 export default Svg;
