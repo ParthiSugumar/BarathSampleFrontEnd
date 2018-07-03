37a7f6fa8d00088aeeab8a2e2fc4a6d5d6ab713d
Parthiban Sugumar <parthibansugumar66@gmail.com>
2018-07-03 22:42:11 +0530

    onHover changed to onClick with return to original state added

--- src/svg.js
+++ src/svg.js
@@ -8,50 +8,66 @@ var unSafeStyle = {
     fill: 'red'
 }
 class Svg extends Component {
+    id1 = "";
+    px1 = 0;
+    py1 = 0;
+    h1 = 0;
+    w1 = 0;
     scale = Math.min(Math.floor(this.props.totalHeight / this.props.LayoutHeight),
         Math.floor(this.props.totalWidth / this.props.LayoutWidth));
     render() {
-        return <svg
-            height={this.props.totalHeight.toString()}
-            width={this.props.totalWidth.toString()}>
-            {
-                this.props.machine.map(machine =>
-                    <g onMouseOver={() => this.rotateLogo(machine.machineID)}>
-                        <svg
-                            id={machine.machineID}
-                            width={(machine.machineWidth * this.scale).toString()}
-                            height={(machine.machineHeight * this.scale).toString()}
-                            x={(machine.pointX * this.scale).toString()}
-                            y={(machine.pointY * this.scale).toString()}
-                            onMouseLeave={() => this.returnLogo(machine.machineID,
-                                (machine.pointX * this.scale).toString(),
-                                (machine.pointY * this.scale).toString(),
-                                (machine.machineHeight * this.scale).toString(),
-                                (machine.machineWidth * this.scale).toString()
-                            )}
-                            dangerouslySetInnerHTML={{ __html: machine.svgString }} />
-                        <svg>
-                            <circle
-                                class={machine.machineID}
-                                cx={(machine.pointX + machine.machineWidth) * this.scale}
-                                cy={machine.pointY * this.scale}
-                                r="10"
-                                style={
-                                    machine.machineStatus === 1 ?
-                                        safeStyle : unSafeStyle
-                                } />
-                            {
-                                machine.machineStatus === 0 ?
-                                    this.alarmSignal(machine.machineID) :
-                                    undefined
-                            }
-                        </svg>
-                    </g>
-                )
-            }
-        </svg>
+        return <div>
+            <svg
+                height={this.props.totalHeight.toString()}
+                width={this.props.totalWidth.toString()}>
+                {
+                    this.props.machine.map(machine =>
+                        <g onClick={() => this.rotateLogo(machine.machineID,
+                            (machine.pointX * this.scale).toString(),
+                            (machine.pointY * this.scale).toString(),
+                            (machine.machineHeight * this.scale).toString(),
+                            (machine.machineWidth * this.scale).toString()
+                        )}>
+                            <svg
+                                id={machine.machineID}
+                                width={(machine.machineWidth * this.scale).toString()}
+                                height={(machine.machineHeight * this.scale).toString()}
+                                x={(machine.pointX * this.scale).toString()}
+                                y={(machine.pointY * this.scale).toString()}
+                                dangerouslySetInnerHTML={{ __html: machine.svgString }} />
+                            <svg>
+                                <circle
+                                    class={machine.machineID}
+                                    cx={(machine.pointX + machine.machineWidth) * this.scale}
+                                    cy={machine.pointY * this.scale}
+                                    r="10"
+                                    style={
+                                        machine.machineStatus === 1 ?
+                                            safeStyle : unSafeStyle
+                                    } />
+                                {
+                                    machine.machineStatus === 0 ?
+                                        this.alarmSignal(machine.machineID) :
+                                        undefined
+                                }
+                            </svg>
+                        </g>
+                    )
+                }
+            </svg>
+            <div style={{ position: "absolute", left: this.props.totalWidth, top: 0 }}>
+                <button
+                    onClick={() => this.returnLogo(this.id1, this.px1, this.py1, this.h1, this.w1)}>X
+                </button>
+            </div>
+        </div>
     }
-    rotateLogo(id) {
+    rotateLogo(id, px, py, h, w) {
+        this.id1 = id;
+        this.px1 = px;
+        this.py1 = py;
+        this.h1 = h;
+        this.w1 = w;
         d3.select("#" + id)
             .attr("x", 0)
             .attr("y", 0)
